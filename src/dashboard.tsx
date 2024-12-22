dbbf2622b908508b67fa97c6a3b15930e986c1f4
Davon Wilson <davontech3@gmail.com>
2024-12-22 18:32:31 -0500

    started upload file design

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 import './app.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpFromBracket } from '@fortawesome/pro-light-svg-icons';
+import { faArrowUpFromBracket, faChartLineUp, faDatabase, faFileChartPie } from '@fortawesome/pro-light-svg-icons';
 import { faWrench } from '@fortawesome/pro-light-svg-icons';
-import { faMessageBot } from '@fortawesome/pro-light-svg-icons';
+import { faHouse } from '@fortawesome/pro-light-svg-icons';
 import { faChartColumn } from '@fortawesome/pro-light-svg-icons';
 import { faFolderOpen } from '@fortawesome/pro-light-svg-icons';
 import { faMicrochipAi } from '@fortawesome/pro-light-svg-icons';
@@ -13,38 +13,53 @@ import { faMessageDots } from '@fortawesome/pro-light-svg-icons';
 import { faQuestion } from "@fortawesome/pro-light-svg-icons";
 import Chatbot from "./components/chatbot";
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faLightbulbOn} from '@fortawesome/pro-light-svg-icons';
 
 
 
 
 
-function Dashboard () {
+function Dashboard() {
 
-    
     return (
         <div className="app-page">
-            
-            
+
+
             <div className="dashboard-view">
                 <div className="menu">
+                    <div className="logo">
+                        <h2>Zorva</h2>
+                        <div className="logo-pic"></div>
+                    </div>
                     <div className="icon">
-                        <FontAwesomeIcon className="menu-icon" icon={faMessageBot as IconProp} />
-                        <p>Chat</p>
+                        <FontAwesomeIcon className="menu-icon" icon={faHouse as IconProp} />
+                        <p>Dashboard</p>
                     </div>
                     <div className="icon">
                         <FontAwesomeIcon className="menu-icon" icon={faArrowUpFromBracket as IconProp} />
                         <p>Upload</p>
                     </div>
-                   
+
                     <div className="icon">
-                        <FontAwesomeIcon className="menu-icon" icon={faMessageDots as IconProp} />
-                        <p>Conversations</p>
+                        <FontAwesomeIcon className="menu-icon" icon={faDatabase as IconProp} />
+                        <p>Datasets</p>
+                    </div>
+                    <div className="icon">
+                        <FontAwesomeIcon className="menu-icon" icon={faLightbulbOn as IconProp} />
+                        <p>Insights</p>
+                    </div>
+                    <div className="icon">
+                        <FontAwesomeIcon className="menu-icon" icon={faFileChartPie as IconProp} />
+                        <p>Reports</p>
+                    </div>
+                    <div className="icon">
+                        <FontAwesomeIcon className="menu-icon" icon={faChartLineUp as IconProp} />
+                        <p>Graphs</p>
                     </div>
                     <div className="icon">
                         <FontAwesomeIcon className="menu-icon" icon={faWrench as IconProp} />
                         <p>Integrations</p>
                     </div>
-                 
                     <div className="icon">
                         <FontAwesomeIcon className="menu-icon" icon={faGear as IconProp} />
                         <p>Settings</p>
@@ -53,8 +68,9 @@ function Dashboard () {
                         <FontAwesomeIcon className="menu-icon" icon={faQuestion as IconProp} />
                         <p>Help</p>
                     </div>
+
                 </div>
-                <div className="dashboard"> 
+                <div className="dashboard">
                     <Chatbot />
                 </div>
             </div>
